9b7458256cecb51b0b8915e5a2037bd3a2a0b5ff
Murrium123 <zaheemur@sheridancollege.ca>
2021-10-09 20:21:57 -0400

    Fifth Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,14 +8,10 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-        Card card = new Card("Diamonds",1);
-
-
-        Card card1 = new Card("Hearts",2);
-   		
-	        System.out.println(" "+ card);
-	        System.out.println(" ");
-                System.out.println(card1);
+        Card card = new Card("Diamonds",1); 
+        System.out.print(" ");
+        System.out.print(card);
+        
     }
-    
+
 }
